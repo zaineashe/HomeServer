0265a9149d416d36b417e1c9ca93eeca4115e9a1
GIT Admin <sigma_blight@outlook.com>
2016-06-06 12:01:45 +1000

    Main Structure in CONNECTION main.cpp and main.h

---
+++ CONNECTION/main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+Main::Main()
+{
+	std::cout << "Initilising Host Machine Home Server" << std::endl;
+}
+
+
+
+
+int main()
+{
+	Main root;
+}
---
+++ CONNECTION/main.h
@@ -0,0 +1,10 @@
+
+// Libraries
+#include <iostream>
+//#include <boost/thread/thread.hpp>
+
+class Main
+{
+public:
+	Main();
+};
