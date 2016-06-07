1d762019fcfe810b729c929c87eab79cfae6c6f1
sigma <sigma_blight@outlook.com>
2016-06-08 08:57:32 +1000

    Restructured 'main.cpp' with function prototypes

--- CONNECTION/V_0.2/main.cpp
+++ CONNECTION/V_0.2/main.cpp
@@ -2,6 +2,36 @@
 #include "GUI.h"
 #include "Server.h"
 
+//************ FUNCTION PROTOTYPES ************
+
+void waitForConnection	(GUI*, Server*);
+void waitForStream		(GUI*, Server*);
+void waitForBuffer		(GUI*, Server*);
+
+//************ MAIN FUNCTION ************
+
+int main()
+{
+	LOG("Initilising Home Server V_0.2");
+	
+	GUI 	gui;
+	Server 	server;
+	
+	gui.setMessage("Waiting For Connection");
+	
+	boost::thread t_gui 	(boost::bind(&GUI::start, &gui));
+	boost::thread t_server	(boost::bind(&Server::start, &server));
+	
+	waitForConnection(&gui, &server);
+	waitForStream(&gui, &server);
+	waitForBuffer(&gui, &server);
+	
+	t_gui.join();
+	t_server.join();
+}
+
+//************ FUNCTION IMPLEMENTATIONS ************
+
 void waitForConnection(GUI* gui, Server* server)
 {
 	while(true)
@@ -28,21 +58,7 @@ void waitForStream(GUI* gui, Server* server)
 	}
 }
 
-int main()
+void waitForBuffer(GUI* gui, Server* server)
 {
-	LOG("Initilising Home Server V_0.2");
 	
-	GUI 	gui;
-	Server 	server;
-	
-	gui.setMessage("Waiting For Connection");
-	
-	boost::thread t_gui 	(boost::bind(&GUI::start, &gui));
-	boost::thread t_server	(boost::bind(&Server::start, &server));
-	
-	waitForConnection(&gui, &server);
-	waitForStream(&gui, &server);
-	
-	t_gui.join();
-	t_server.join();
 }
